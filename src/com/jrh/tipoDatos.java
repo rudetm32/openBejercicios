223898e704d77f67d9bbfca5d5b376a58590b1b3
rudetm32 <jaruiz721818@gmail.com>
2022-07-01 15:35:59 -0500

    Java tema2 ejercicio1

---
+++ src/com/jrh/tipoDatos.java
@@ -0,0 +1,33 @@
+package com.jrh;
+
+public class tipoDatos {
+
+    public static void main(String[] args) {
+    /*Para este primer ejercicio tendréis que realizar lo siguiente:
+    *Crea un proyecto de Java desde 0
+    *Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+    *Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+    */
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 50;
+        long variable4 = 100;
+        float variable5 = 3.5f;
+        double variable6 = 10.55d;
+        boolean variable7 = false;
+        char variable8 = 'a';
+        String variable9 = "Hola mundo!";
+
+        System.out.printf("tipo byte       " + variable1 + "\n");
+        System.out.printf("tipo short      " + variable2 + "\n");
+        System.out.printf("tipo int        " + variable3 + "\n");
+        System.out.printf("tipo long       " + variable4 + "\n");
+        System.out.printf("tipo float      " + variable5 + "\n");
+        System.out.printf("tipo double     " + variable6 + "\n");
+        System.out.printf("tipo boleano    " + variable7 + "\n");
+        System.out.printf("tipo char       " + variable8 + "\n");
+        System.out.printf("tipo String     " + variable9 + "\n");
+
+
+    }
+}
