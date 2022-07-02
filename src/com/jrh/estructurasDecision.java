acd7ae2c6d0d2c1709561d5cf4e2edeb8b917a0a
rudetm32 <jaruiz721818@gmail.com>
2022-07-01 22:20:09 -0500

    estructuras de control ejercicio tema 3

---
+++ src/com/jrh/estructurasDecision.java
@@ -0,0 +1,20 @@
+package com.jrh;
+
+public class estructurasDecision {
+
+    public static void main(String[] args) {
+       /*
+        * En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima
+        * el resultado final por consola.
+        * Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
+        * String[] nombres = {"", "", "", ""}
+        */
+        String[] nombres = {"pedro", "luis", "jose", "viridiana"};
+        String suma = "";
+        for(int i = 0; i<nombres.length; i++){
+            suma += nombres[i];
+
+        }
+        System.out.println(suma);
+         }
+}
